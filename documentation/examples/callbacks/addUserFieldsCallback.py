ad256392d2bf2ed019749c2d5f691ace70404fce
Nathan Dunsworth <nathandunsworth@gmail.com>
2013-08-12 05:11:07 -0700

    Updated example SearchExpressionField and added callback example
    - Updated the SearchExpressionField example with the new field changes
    
    - Added new example file that shows hooking into the callback system

--- documentation/examples/callbacks/addUserFieldsCallback.py
+++ documentation/examples/callbacks/addUserFieldsCallback.py
@@ -25,72 +25,68 @@
 #
 
 __all__ = [
-  'SearchExpressionField'
+  'addUserFieldsCallback'
 ]
-
-# Python imports
-import copy
 
 # This module imports
 import ShotgunORM
 
-class SearchExpressionField(ShotgunORM.SgFieldEntityMulti):
-  '''
-  An example user field that performs a Shotgun search for its return result.
+################################################################################
+#
+# The custom field that will be added to all ApiUser Entity objects.
+#
+################################################################################
 
-  Finds all HumanUser Entities that are not named "Bob Smith".
+class ApiUserNameField(ShotgunORM.SgFieldText):
+  '''
+  Field that returns the value of the "firstname" and "lastname".
   '''
 
-  def __init__(self, name, label=None):
-    super(SearchExpressionField, self).__init__(name, label)
+  def __init__(self):
+    super(ApiUserNameField, self).__init__('name')
 
     # Don't allow the field to be modified!
     self.schemaInfo().setEditable(False)
 
-    self._searchEntityType = 'HumanUser'
-
-    self._searchFilters = [
-      [
-        'name',
-        'is_not',
-        'Bob Smith'
-      ]
-    ]
-
-  def searchFilters(self):
-    '''
-    Returns the search filters used for the Shotgun search.
-    '''
-
-    if self._searchFilters == None:
-      return None
-
-    return copy.deepcopy(self._searchFilters)
-
-  def searchEntityType(self):
-    '''
-    Returns the Entity type that this field searches for.
-    '''
-
-    return self._searchEntityType
-
   def _valueSg(self):
-    '''
-    Performs the Shotgun search for this field and returns the result.
-
-    The return result of this is the value that the field will return.
-    '''
-
     parent = self.parentEntity()
-
-    print parent
 
     if parent == None:
       return None
 
-    sgSearch = parent.connection()._sg_find(
-      self.searchEntityType(),
-      self.searchFilters()
-    )
+    values = parent.fieldValues(['firstname', 'lastname'])
 
-    return sgSearch
+    return '%(firstname)s %(lastname)s' % values
+
+################################################################################
+#
+# The callback that will be registered to onEntityCreate() and will add the
+# name field to ApiUser Entity objects.
+#
+################################################################################
+
+def addApiUserNameField(sgEntity):
+  '''
+  This example callback shows how to add user fields to Entity objects as they
+  are created.
+
+  A SgUserField will be created for all ApiUser Entity objects that adds a name
+  field for the Entity.  ApiUsers do not contain a "name" field as part of the
+  Shotgun schema.
+  '''
+
+  if sgEntity.type == 'ApiUser':
+    if not sgEntity.hasField('name'):
+      sgEntity.addField(ApiUserNameField())
+
+################################################################################
+#
+# Register the callback with ShotgunORM onEntityCreate().
+#
+################################################################################
+
+ShotgunORM.addOnEntityCreate(
+  addApiUserNameField,
+  filterName='ApiUser',
+  description='callback that adds a name field to ApiUser Entity objects'
+)
