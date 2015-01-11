baa7c708da4284d7ce9ca68be8e04ffe21f8c1a4
Nathan Dunsworth <nathandunsworth@gmail.com>
2015-01-11 13:03:17 -0500

    SgQueryEngine renamed SgJob class to SgQueryJob

--- ShotgunORM/SgQueryEngine.py
+++ ShotgunORM/SgQueryEngine.py
@@ -35,7 +35,7 @@ import weakref
 # This module imports
 import ShotgunORM
 
-class SgJob(object):
+class SgQueryJob(object):
   '''
 
   '''
@@ -214,7 +214,7 @@ class SgQueryEngine(object):
           # around.
           entities = [weakref.ref(sgEntity)]
 
-          q = SgJob(t, entities, pullFields)
+          q = SgQueryJob(t, entities, pullFields)
 
           eq.append(q)
 
@@ -263,7 +263,7 @@ class SgQueryEngine(object):
           if len(pullFields) >= 1:
             entities = [weakref.ref(sgEntity)]
 
-            q = SgJob(t, entities, pullFields)
+            q = SgQueryJob(t, entities, pullFields)
 
             eq.append(q)
 
